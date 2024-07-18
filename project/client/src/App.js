d4f51b4f6d02abcf85ae8b6802f2c00b13d7e20d
Dipti Hatwar <kashu@localhost.localdomain>
2024-07-18 21:55:54 +0530

    Changes made in User and admin routes

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -38,9 +38,9 @@ function App() {
           <Route
             path="/admin"
             element={
-              <ProtectedRoute>
+             // <ProtectedRoute>
                 <Admin />
-              </ProtectedRoute>
+             // </ProtectedRoute>
             }
           />
           <Route
